1bb3583493a96376d6faa82c92f960941217b986
JULIAN MARCOZZI <julianmarcozzi@gmail.com>
2026-04-29 14:26:30 -0300

    Initial commit

---
+++ Assets/Scripts/InteractiveAreaScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveAreaScript : MonoBehaviour
+{
+    public ScoreUIManager ScoreUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision col) 
+    {
+        if(col.gameObject.CompareTag("Pickable")) 
+        {
+            Destroy(col.gameObject);
+            ScoreUI.AddPointAndUpdateScore();
+        }
+    }
+}
---
+++ Assets/Scripts/ScoreUIManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreUIManager : MonoBehaviour
+{
+    public TextMeshProUGUI txtScore;
+    public int score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtScore.text = score.ToString();
+    }
+
+    public void AddPointAndUpdateScore() 
+    {
+        score ++;
+        txtScore.text = score.ToString();
+        if(score >= 3)
+    {
+        txtScore.text = "GANASTE";
+    }
+    else
+    {
+        txtScore.text = score.ToString();
+    }
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
